5c2b50c721d5435aa16482bdcdf3b464a8a1aac6
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-06-16 21:42:51 +0200

    build: add CSS optimization plugins and browserslist config
    - Added autoprefixer, cssnano, postcss-purgecss and combine selectors
    - Created .browserslistrc to define target browsers
    - Updated package.json and package-lock.json with new dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 // postcss.config.js
 import pxtorom from '@minko-fe/postcss-pxtorem';
+import autoprefixer from 'autoprefixer';
+import combineDuplicatedSelectors from 'postcss-combine-duplicated-selectors';
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import cssnano from 'cssnano';
 
 export default {
   plugins: [
@@ -7,6 +11,18 @@ export default {
       rootValue: 16,
       propList: ['*'],
       atRules: ['media'],
+    }),
+    autoprefixer(),
+    combineDuplicatedSelectors(),
+    purgeCSSPlugin({
+      content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue,html}'],
+      safelist: {
+        standard: [/^important-/, 'always-keep-this-class'],
+      },
+      variables: true,
+    }),
+    cssnano({
+      preset: 'default',
     }),
   ],
 };
