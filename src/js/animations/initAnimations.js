5313f45554031bf6852ca86da08ab14255223eb9
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-06-17 10:14:19 +0200

    refactor: remove unused gsap imports and code

--- src/js/animations/initAnimations.js
+++ src/js/animations/initAnimations.js
@@ -1,49 +1,4 @@
-import { gsap } from 'gsap';
-import { CustomEase } from 'gsap/CustomEase';
-import { CustomBounce } from 'gsap/CustomBounce';
-import { CustomWiggle } from 'gsap/CustomWiggle';
-
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
-import { GSDevTools } from 'gsap/GSDevTools';
-
 export function initAnimations() {
-  gsap.registerPlugin(
-    GSDevTools,
-    ScrollTrigger,
-    ScrollSmoother,
-    ScrollToPlugin,
-    CustomEase,
-    CustomBounce,
-    CustomWiggle,
-  );
-
   console.log(window.matchMedia('(prefers-reduced-motion: reduce)').matches);
   console.log(window.matchMedia('(prefers-reduced-motion: no-preference)').matches);
-
-  /*if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;*/
-
-  /*let mm = gsap.matchMedia();
-  mm.add("(prefers-reduced-motion: no-preference)", () => {
-    // Full animation here
-  });
-  mm.add("(prefers-reduced-motion: reduce)", () => {
-    // Reduced or no animation here
-  });*/
-
-  /*gsap.to('.noice', {
-    scrollTrigger: {
-      trigger: '.body',
-      start: 'top',
-      end: '20%',
-      scrub: 0.5,
-      markers: true,
-    },
-    y: 300,
-    x: 100,
-    opacity: 1,
-    ease: 'power1.out',
-  });*/
 }
